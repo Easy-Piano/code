422e427520ca6debab8efb09cce495dc1c40f3dd
Dmitry Grinko <grinko-d@mail.ru>
2014-05-15 23:28:17 +0400

    small change

--- ru/mipt/cs/recognition/analysis/Notes.java
+++ ru/mipt/cs/recognition/analysis/Notes.java
@@ -124,7 +124,7 @@ public class Notes implements Serializable{//is also a Singleton
     }
     private static Notes getInstanceFromFile(){
         try {
-            if (wasInitialized == false) {
+            if (!wasInitialized) {
                 if (!((new File(saveName)).exists())) {
                     Notes notes = new Notes();
                     instance = notes;
@@ -228,9 +228,7 @@ public class Notes implements Serializable{//is also a Singleton
             System.out.println("dp = "+dp);*/
             System.out.println("note "+noteNumber+" spectrum received");
             save();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (IndexOutOfBoundsException e){
+        } catch (IOException | IndexOutOfBoundsException e) {
             e.printStackTrace();
         }
     }
