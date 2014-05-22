0b99d4292ad6ad9a2ac8f6ffef89d533fb275366
restvik <restvik@gmail.com>
2014-05-22 08:28:36 +0400

    constants

--- ru/mipt/cs/easypiano/piano/Constants.java
+++ ru/mipt/cs/easypiano/piano/Constants.java
@@ -11,27 +11,13 @@ import java.awt.Font;
 import java.awt.event.KeyEvent;
 
 public class Constants {
-<<<<<<< HEAD
+
 	// Video
     public static final int CANVAS_MINI_HEIGHT = 400;
     public static final int CANVAS_MINI_WIDTH = 1260;
     public static final int CANVAS_WHITE_KEY_WIDTH = 30;
     public static final int CANVAS_BLACK_KEY_WIDTH = 24;
 
-
-
-
-    // piano
-	public static final Color PIANO_BACKGROUND_COLOR = new Color(0.62352943f, 0.7019608f, 0.69411767f);
-
-    public static final boolean BLACK_COLOR = true;
-    public static final boolean WHITE_COLOR = false;
-	
-	public static final int WHITE_KEY_WIDTH = 63;
-	public static final int WHITE_KEY_HEIGHT = 280;
-	public static final int BLACK_KEY_WIDTH = 37;
-	public static final int BLACK_KEY_HEIGHT = 173;
-=======
 	
 	// piano
 	public static final Color PIANO_BACKGROUND_COLOR = new Color(0.6f, 0.7f, 0.7f);
@@ -40,7 +26,6 @@ public class Constants {
 	public static final int WHITE_KEY_HEIGHT = ImageManager.getInstance(ImageManager.WHITE_KEY_CENTRAL_UP).getHeight();
 	public static final int BLACK_KEY_WIDTH = ImageManager.getInstance(ImageManager.BLACK_KEY_UP).getWidth();
 	public static final int BLACK_KEY_HEIGHT = ImageManager.getInstance(ImageManager.BLACK_KEY_UP).getHeight();
->>>>>>> b86198ecac09c40245af3b6b9b6e4b06a68f68a0
 	public static final int KEY_LEFT_OFFSET = WHITE_KEY_WIDTH + 1;
 	public static final int KEY_FRAME_HEIGHT = WHITE_KEY_HEIGHT + 2;
 	
@@ -71,3 +56,16 @@ public class Constants {
     static final int NUM_OCTAVES = 6;
     static final int NUM_KEYS = NUM_KEYS_PER_OCTAVE* NUM_OCTAVES + 1;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
