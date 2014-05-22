6e5f6020e1e8e18e537b4e2f37dba92651352388
EroshenkoA <furysham@mail.ru>
2014-05-22 23:52:30 +0400

    croissants vector assigned
    first real interaction

--- ru/mipt/cs/easypiano/graphics/videolesson/video/Canvas.java
+++ ru/mipt/cs/easypiano/graphics/videolesson/video/Canvas.java
@@ -3,20 +3,22 @@ package ru.mipt.cs.easypiano.graphics.videolesson.video;
 // Ivan
 
 import ru.mipt.cs.easypiano.graphics.videolesson.Croissant;
+import ru.mipt.cs.easypiano.recognition.aggregation.frommidi.NotesExtractor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Vector;
 
 public class Canvas extends JPanel{
-    private Croissant c = new Croissant();
+    private Vector croissants;//Croissant c = new Croissant();
     private int width;
     private int height;
     private  boolean running = true;
-
-    public Canvas (){
-
-        
+    private String nameOfFile;
+    public Canvas (String fileName){
+        nameOfFile=fileName;
         //layouting
+        nameOfFile=fileName;
         this.width = VideoConstants.CANVAS_MINI_WIDTH;
         this.height = VideoConstants.CANVAS_MINI_HEIGHT;
         setPreferredSize(new Dimension(width, height));
@@ -27,12 +29,38 @@ public class Canvas extends JPanel{
         c.setStartTick(0);
         c.start();
         */
+        croissants = new Vector();
+        NotesExtractor NE = new NotesExtractor(nameOfFile);
+        Vector durations = NE.getDurations();
+        Vector notes = NE.getNotes();
+        Vector startTimes = NE.getStartTimes();
+        int n = durations.size();
+        for (int i=0; i<n; i++){
+            croissants.add(new Croissant());
+            ((Croissant)croissants.get(i)).setDuration((int)(long)durations.get(i));
+            ((Croissant)croissants.get(i)).setNote((int)notes.get(i));
+            ((Croissant)croissants.get(i)).setStartTick((int)(long) startTimes.get(i));
+        }
+        startMotion();
+        /*for (int i=0; i<n; i++){
+            System.out.println("note is "+notes.get(i));
+        }*/
+    }
+    private void startMotion(){
+        int n = croissants.size();
+        for (int i=0; i<n; i++){
+            ((Croissant)croissants.get(i)).start();
+        }
     }
 
     public void  paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2;
         g2 = (Graphics2D ) g;
-        c.draw(g2);
+        int n = croissants.size();
+        for (int i=0; i<n; i++){
+            ((Croissant)croissants.get(i)).draw(g2);
+        }
+        //c.draw(g2);
     }
 }
--- ru/mipt/cs/easypiano/recognition/aggregation/frommidi/NotesExtractor.java
+++ ru/mipt/cs/easypiano/recognition/aggregation/frommidi/NotesExtractor.java
@@ -8,105 +8,107 @@ import java.io.IOException;
 import java.util.Vector;
 
 public class NotesExtractor {
-        public static final int NOTE_ON = 0x90;
-        public static final int NOTE_OFF = 0x80;
-        private Vector vecNotes;
-        private Vector vecStartTimes;//time when note starts, in milliseconds.
-        private Vector vecDurations;//duration of note
-        private NoteInfo ni;
-        public NotesExtractor(String fName){
-            vecNotes = new Vector();
-            vecStartTimes = new Vector();
-            vecDurations = new Vector();
-            decodeFile(fName);
-            ni = new NoteInfo();
-            //show();
-        }
-        public NoteInfo getNoteInfo(){
-            return ni;
-        }
-
-        public class NoteInfo{
-            /*
-            example of usage class in main
-            NoteInfo NI = (new NotesExtractor("path_to_file.mid")).getNoteInfo();
-            Vector startTimes = NI.getSTvec(myNoteNumber);
-            Vector endTimes = NI.getETvec(myNoteNumber);
-             */
-            protected Vector[] startTime;
-            protected Vector[] endTime;
-            protected NoteInfo(){
-                startTime = new Vector[Notes.NOTES_QUANTITY];
-                endTime = new Vector[Notes.NOTES_QUANTITY];
-                int n = vecNotes.size();
-                for (int i=0; i<n; i++){
-                    startTime[(int) vecNotes.get(i)].add(vecStartTimes.get(i));
-                    endTime[(int) vecNotes.get(i)].add((long)vecStartTimes.get(i)+(long)vecDurations.get(i));
-                }
-            }
-            public Vector getSTvec(int noteNumber){
-                if ((noteNumber < 0)||(noteNumber > Notes.NOTES_QUANTITY-1)) throw new IndexOutOfBoundsException();
-                return startTime[noteNumber];
-            }
-            public Vector getETvec(int noteNumber){
-                if ((noteNumber < Notes.FIRST_NOTE)||(noteNumber > Notes.LAST_NOTE)) throw new IndexOutOfBoundsException();
-                return endTime[noteNumber];
+    public static final int NOTE_ON = 0x90;
+    public static final int NOTE_OFF = 0x80;
+    private Vector vecNotes;
+    private Vector vecStartTimes;//time when note starts, in milliseconds.
+    private Vector vecDurations;//duration of note
+    private double ticksPerSecond;
+    private NoteInfo ni;
+    public NotesExtractor(String fName){
+        vecNotes = new Vector();
+        vecStartTimes = new Vector();
+        vecDurations = new Vector();
+        ni = new NoteInfo();
+        decodeFile(fName);
+        show();
+    }
+    public class NoteInfo{
+        /*
+        example of usage class in main
+        NoteInfo NI = (new NotesExtractor("path_to_file.mid")).getNoteInfo();
+        Vector startTimes = NI.getSTvec(myNoteNumber);
+        Vector endTimes = NI.getETvec(myNoteNumber);
+         */
+        protected Vector[] startTime;
+        protected Vector[] endTime;
+        protected NoteInfo(){
+            startTime = new Vector[Notes.NOTES_QUANTITY];
+            endTime = new Vector[Notes.NOTES_QUANTITY];
+            int n = vecNotes.size();
+            for (int i=0; i<n; i++){
+                startTime[(int) vecNotes.get(i)].add(vecStartTimes.get(i));
+                endTime[(int) vecNotes.get(i)].add((long)vecStartTimes.get(i)+(long)vecDurations.get(i));
             }
         }
-        public void show(){
-            int n=vecNotes.size();
+        public Vector getSTvec(int noteNumber){
+            if ((noteNumber < 0)||(noteNumber > Notes.NOTES_QUANTITY-1)) throw new IndexOutOfBoundsException();
+            return startTime[noteNumber];
+        }
+        public Vector getETvec(int noteNumber){
+            if ((noteNumber < Notes.FIRST_NOTE)||(noteNumber > Notes.LAST_NOTE)) throw new IndexOutOfBoundsException();
+            return endTime[noteNumber];
+        }
+    }
+    public NoteInfo getNoteInfo() {
+        return ni;
+    }
+    public void show(){
+        int n=vecNotes.size();
             /*int n1=vecDurations.size();
             int n2=vecStartTimes.size();
             System.out.println("note size is "+n+" duration size is "+n1+" startTime size is "+n2);*/
-            for (int i=0; i<n; i++){
-                System.out.println("note: "+vecNotes.get(i)+" duration: "+vecDurations.get(i)+" startTime: "+
-                vecStartTimes.get(i));
-            }
+        for (int i=0; i<n; i++){
+            System.out.println("note: "+vecNotes.get(i)+" duration: "+vecDurations.get(i)+" startTime: "+
+                    vecStartTimes.get(i));
         }
-        public Vector getNotes(){
-            return vecNotes;
+    }
+    public Vector getNotes(){
+        return vecNotes;
+    }
+    public Vector getStartTimes(){
+        return vecStartTimes;
+    }
+    public Vector getDurations(){
+        return vecDurations;
+    }
+    private void decodeFile (String fName) {
+        Sequence sequence = null;
+        try {
+            sequence = MidiSystem.getSequence(new File(fName));
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        public Vector getStartTimes(){
-            return vecStartTimes;
-        }
-        public Vector getDurations(){
-            return vecDurations;
-        }
+        Track track = sequence.getTracks()[1];
 
-        private void decodeFile (String fName){
-            Sequence sequence = null;
-            try {
-                sequence = MidiSystem.getSequence(new File(fName));
-            } catch (InvalidMidiDataException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Track track = sequence.getTracks()[1];
-            for (int i=0; i < track.size(); i++) {
-                MidiEvent event = track.get(i);
-                //System.out.print("@" + event.getTick() + " ");
-                MidiMessage message = event.getMessage();
-                if (message instanceof ShortMessage) {
-                    ShortMessage sm = (ShortMessage) message;
-                    //System.out.print("Channel: " + sm.getChannel() + " ");
-                    if (sm.getCommand() == NOTE_ON && sm.getData2()!=0) {
-                        int key = sm.getData1();
-                        vecNotes.add(key);
-                        vecStartTimes.add(event.getTick());
-                        //int velocity = sm.getData2();
-                    } else if ((sm.getCommand() == NOTE_OFF)||(sm.getCommand() == NOTE_ON && sm.getData2()==0)) {
-                        int key = sm.getData1();
-                        vecDurations.add(event.getTick() - (long) vecStartTimes.get(findLastMention(key)));
-                    }
+        ticksPerSecond = ((double) (1000 * sequence.getTickLength()) / (double) (sequence.getMicrosecondLength()));
+        System.out.println(ticksPerSecond + " ticks per second");
+        for (int i = 0; i < track.size(); i++) {
+            MidiEvent event = track.get(i);
+            //System.out.print("@" + event.getTick() + " ");
+            MidiMessage message = event.getMessage();
+            if (message instanceof ShortMessage) {
+                ShortMessage sm = (ShortMessage) message;
+                //System.out.print("Channel: " + sm.getChannel() + " ");
+                if (sm.getCommand() == NOTE_ON && sm.getData2() != 0) {
+                    int key = sm.getData1();
+                    vecNotes.add(key);
+                    vecStartTimes.add((long) ((double) event.getTick() / ticksPerSecond));
+                    //int velocity = sm.getData2();
+                } else if ((sm.getCommand() == NOTE_OFF) || (sm.getCommand() == NOTE_ON && sm.getData2() == 0)) {
+                    int key = sm.getData1();
+                    vecDurations.add((long) ((double) event.getTick() / ticksPerSecond) - (long) vecStartTimes.get(findLastMention(key)));
                 }
             }
         }
-        private int findLastMention(int key){
-            int n=vecNotes.size()-1;
-            while (((int)(vecNotes.get(n)))!=key){
-                n--;
-            }
-            return n;
+    }
+    private int findLastMention(int key){
+        int n=vecNotes.size()-1;
+        while (((int)(vecNotes.get(n)))!=key){
+            n--;
         }
+        return n;
+    }
 }
